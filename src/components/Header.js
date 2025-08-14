92e0b02cc31105e17661c743abcd88cea053dd9b
Prabin Dangol <dprabin23@gmail.com>
2025-08-14 07:04:43 +0545

    change fav icon
    Signed-off-by: Prabin Dangol <dprabin23@gmail.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-
 import AuthUser from "./AuthUser";
 import Navlink from "./Navlink";
 import logo from "@/assets/images/logo.png";
